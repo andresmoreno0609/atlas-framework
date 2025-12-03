dca415c3d59e772ba9f2b35f01d4b96530a57399
Andres Moreno <andresmoreno1991609@gmail.com>
2025-12-03 14:52:03 -0500

    first commit

---
+++ src/main/java/com/atlas/framework/AtlasFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.atlas.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlasFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlasFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atlas/framework/AtlasFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlas.framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlasFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
